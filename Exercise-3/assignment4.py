dad44f3890745b49e1d4d3191e5acdb8a56ec872
Vladimir Dimitrov Spassov <v_spassov@web.de>
2022-11-17 16:41:08 +0100

    Final state for today, probably still not working as intended.

--- Exercise-3/assignment4.py
+++ Exercise-3/assignment4.py
@@ -29,32 +29,23 @@ def main():
                                                                input_img.shape[0] - (input_img.shape[0] % 8)))
 
     # dct output destination
-    dct_img = np.zeros(shape=input_img.shape, dtype=np.float32)
+    dct_img = np.zeros(shape=input_imgf_normalized.shape, dtype=np.float32)
 
     multiplication_constants = [x for x in range(-5, 6) if x != 0]
     addition_constants = [x for x in range(-250, 300, 50) if x != 0]
 
-    multiplication_dct_quadrant1_img = np.zeros(shape=input_img.shape, dtype=np.float32)
-    multiplication_dct_quadrant2_img = np.zeros(shape=input_img.shape, dtype=np.float32)
-    multiplication_dct_quadrant3_img = np.zeros(shape=input_img.shape, dtype=np.float32)
-    multiplication_dct_quadrant4_img = np.zeros(shape=input_img.shape, dtype=np.float32)
-    addition_dct_quadrant1_img = np.zeros(shape=input_img.shape, dtype=np.float32)
-    addition_dct_quadrant2_img = np.zeros(shape=input_img.shape, dtype=np.float32)
-    addition_dct_quadrant3_img = np.zeros(shape=input_img.shape, dtype=np.float32)
-    addition_dct_quadrant4_img = np.zeros(shape=input_img.shape, dtype=np.float32)
-
-    # dct images
-    dct_img_1_quadrant = np.zeros(shape=input_img.shape, dtype=np.float32)
-
     # precomputed dct image
-    dct_img = np.zeros(shape=input_img.shape, dtype=np.float32)
+    dct_img[:, :, 0] = cv2.dct(input_imgf_normalized[:, :, 0])
+    dct_img[:, :, 1] = cv2.dct(input_imgf_normalized[:, :, 1])
+    dct_img[:, :, 2] = cv2.dct(input_imgf_normalized[:, :, 2])
 
-    for x in range(0, input_imgf_normalized.shape[0] - 8, 8):
-        for y in range(0, input_imgf_normalized.shape[1] - 8, 8):
-            for rgb_channel in range(0, 3):
-                dct_img[x:x + 8, y:y + 8, rgb_channel] = cv2.dct(input_imgf_normalized[x:x + 8, y:y + 8, rgb_channel])
+    #for x in range(0, input_imgf_normalized.shape[0] - 8, 8):
+    #    for y in range(0, input_imgf_normalized.shape[1] - 8, 8):
+    #        for rgb_channel in range(0, 3):
+    #            dct_img[x:x + 8, y:y + 8, rgb_channel] = cv2.dct(input_imgf_normalized[x:x + 8, y:y + 8, rgb_channel])
 
-    # create a plot
+
+    # create the plots
     fig1 = plt.figure()
     fig2 = plt.figure()
 
@@ -72,7 +63,7 @@ def main():
         multiplication_idct_quadrant1_img = \
             multiplication_idct_quadrant2_img = \
             multiplication_idct_quadrant3_img = \
-            multiplication_idct_quadrant4_img = np.zeros(shape=input_img.shape, dtype=np.float32)
+            multiplication_idct_quadrant4_img = np.zeros(shape=input_imgf_normalized.shape, dtype=np.float32)
         for x_origin in range(0, input_imgf_normalized.shape[0] - 8, 8):
             for y_origin in range(0, input_imgf_normalized.shape[1] - 8, 8):
                 for rgb_channel in range(0, 3):
@@ -80,73 +71,95 @@ def main():
                     # quadrant 1 manipulation
                     for x_window in range(4, 8):
                         for y_window in range(0, 4):
-                            multiplication_dct_quadrant1_img[x_window, y_window] = constant * \
-                                                                                   multiplication_dct_quadrant1_img[
-                                                                                       x_origin + x_window,
-                                                                                       y_origin + y_window]
+                            multiplication_dct_quadrant1_img[x_window, y_window, rgb_channel] = constant * \
+                                                                                                multiplication_dct_quadrant1_img[
+                                                                                                    x_origin + x_window,
+                                                                                                    y_origin + y_window,
+                                                                                                    rgb_channel]
 
                     # quadrant 2 manipulation
                     for x_window in range(4, 8):
                         for y_window in range(4, 8):
-                            multiplication_dct_quadrant2_img[x_window, y_window] = constant * \
-                                                                                   multiplication_dct_quadrant1_img[
-                                                                                       x_origin + x_window,
-                                                                                       y_origin + y_window]
+                            multiplication_dct_quadrant2_img[x_window, y_window, rgb_channel] = constant * \
+                                                                                                multiplication_dct_quadrant2_img[
+                                                                                                    x_origin + x_window,
+                                                                                                    y_origin + y_window,
+                                                                                                    rgb_channel]
 
                     # quadrant 3 manipulation
                     for x_window in range(0, 4):
                         for y_window in range(4, 8):
-                            multiplication_dct_quadrant2_img[x_window, y_window] = constant * \
-                                                                                   multiplication_dct_quadrant1_img[
-                                                                                       x_origin + x_window,
-                                                                                       y_origin + y_window]
+                            multiplication_dct_quadrant3_img[x_window, y_window, rgb_channel] = constant * \
+                                                                                                multiplication_dct_quadrant3_img[
+                                                                                                    x_origin + x_window,
+                                                                                                    y_origin + y_window,
+                                                                                                    rgb_channel]
 
                     # quadrant 4 manipulation
                     for x_window in range(0, 4):
                         for y_window in range(0, 4):
-                            multiplication_dct_quadrant2_img[x_window, y_window] = constant * \
-                                                                                   multiplication_dct_quadrant1_img[
-                                                                                       x_origin + x_window,
-                                                                                       y_origin + y_window]
+                            multiplication_dct_quadrant4_img[x_window, y_window, rgb_channel] = constant * \
+                                                                                                multiplication_dct_quadrant4_img[
+                                                                                                    x_origin + x_window,
+                                                                                                    y_origin + y_window,
+                                                                                                    rgb_channel]
 
                     # inverse dct
-                    multiplication_idct_quadrant1_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel] = multiplication_dct_quadrant1_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel]
-                    multiplication_idct_quadrant2_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel] = multiplication_dct_quadrant2_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel]
-                    multiplication_idct_quadrant3_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel] = multiplication_dct_quadrant3_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel]
-                    multiplication_idct_quadrant4_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel] = multiplication_dct_quadrant4_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel]
+                    #multiplication_idct_quadrant1_img[x_origin:x_origin + 8, y_origin:y_origin + 8,
+                    #rgb_channel] = cv2.idct(
+                    #    multiplication_dct_quadrant1_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel])
+                    #multiplication_idct_quadrant2_img[x_origin:x_origin + 8, y_origin:y_origin + 8,
+                    #rgb_channel] = cv2.idct(
+                    #    multiplication_dct_quadrant2_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel])
+                    #multiplication_idct_quadrant3_img[x_origin:x_origin + 8, y_origin:y_origin + 8,
+                    #rgb_channel] = cv2.idct(
+                    #    multiplication_dct_quadrant3_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel])
+                    #multiplication_idct_quadrant4_img[x_origin:x_origin + 8, y_origin:y_origin + 8,
+                    #rgb_channel] = cv2.idct(
+                    #    multiplication_dct_quadrant4_img[x_origin:x_origin + 8, y_origin:y_origin + 8, rgb_channel])
+
+        # inverse dct for every colour channel
+        multiplication_idct_quadrant1_img[:, :, 0] = cv2.idct(multiplication_dct_quadrant1_img[:, :, 0])
+        multiplication_idct_quadrant2_img[:, :, 0] = cv2.idct(multiplication_dct_quadrant2_img[:, :, 0])
+        multiplication_idct_quadrant3_img[:, :, 0] = cv2.idct(multiplication_dct_quadrant3_img[:, :, 0])
+        multiplication_idct_quadrant4_img[:, :, 0] = cv2.idct(multiplication_dct_quadrant4_img[:, :, 0])
+
+        multiplication_idct_quadrant1_img[:, :, 1] = cv2.idct(multiplication_dct_quadrant1_img[:, :, 1])
+        multiplication_idct_quadrant2_img[:, :, 1] = cv2.idct(multiplication_dct_quadrant2_img[:, :, 1])
+        multiplication_idct_quadrant3_img[:, :, 1] = cv2.idct(multiplication_dct_quadrant3_img[:, :, 1])
+        multiplication_idct_quadrant4_img[:, :, 1] = cv2.idct(multiplication_dct_quadrant4_img[:, :, 1])
+
+        multiplication_idct_quadrant1_img[:, :, 2] = cv2.idct(multiplication_dct_quadrant1_img[:, :, 2])
+        multiplication_idct_quadrant2_img[:, :, 2] = cv2.idct(multiplication_dct_quadrant2_img[:, :, 2])
+        multiplication_idct_quadrant3_img[:, :, 2] = cv2.idct(multiplication_dct_quadrant3_img[:, :, 2])
+        multiplication_idct_quadrant4_img[:, :, 2] = cv2.idct(multiplication_dct_quadrant4_img[:, :, 2])
 
         # convert images from normalized floats to rgb uint8
-        multiplication_idct_quadrant1_img = (multiplication_dct_quadrant1_img * 255).astype(np.uint8)
-        multiplication_idct_quadrant2_img = (multiplication_dct_quadrant2_img * 255).astype(np.uint8)
-        multiplication_idct_quadrant3_img = (multiplication_dct_quadrant3_img * 255).astype(np.uint8)
-        multiplication_idct_quadrant4_img = (multiplication_dct_quadrant4_img * 255).astype(np.uint8)
-
+        multiplication_idct_quadrant1_img = (multiplication_idct_quadrant1_img * 255).astype(np.uint8)
+        multiplication_idct_quadrant2_img = (multiplication_idct_quadrant2_img * 255).astype(np.uint8)
+        multiplication_idct_quadrant3_img = (multiplication_idct_quadrant3_img * 255).astype(np.uint8)
+        multiplication_idct_quadrant4_img = (multiplication_idct_quadrant4_img * 255).astype(np.uint8)
 
         # plot images
-        ax1 = fig1.add_subplot(10, 5, i*5+1)
+        ax1 = fig1.add_subplot(10, 5, i * 5 + 1)
         ax1.imshow(multiplication_idct_quadrant1_img)
         ax1.set_title("Quadrant 1, Multiplication: " + str(constant), fontsize="30")
-        ax1 = fig1.add_subplot(10, 5, i*5+2)
+        ax1 = fig1.add_subplot(10, 5, i * 5 + 2)
         ax1.imshow(multiplication_idct_quadrant2_img)
         ax1.set_title("Quadrant 2, Multiplication: " + str(constant), fontsize="30")
-        ax1 = fig1.add_subplot(10, 5, i*5+3)
+        ax1 = fig1.add_subplot(10, 5, i * 5 + 3)
         ax1.imshow(input_img)
         ax1.set_title("Original Image", fontsize="30")
-        ax1 = fig1.add_subplot(10, 5, i*5+4)
+        ax1 = fig1.add_subplot(10, 5, i * 5 + 4)
         ax1.imshow(multiplication_idct_quadrant3_img)
         ax1.set_title("Quadrant 3, Multiplication: " + str(constant), fontsize="30")
-        ax1 = fig1.add_subplot(10, 5, i*5+5)
+        ax1 = fig1.add_subplot(10, 5, i * 5 + 5)
         ax1.imshow(multiplication_idct_quadrant4_img)
         ax1.set_title("Quadrant 4, Multiplication: " + str(constant), fontsize="30")
 
-
-
-
-
-
     # fig.tight_layout()
     fig1.show()
-    fig1.savefig("output.pdf", dpi=500)
+    fig1.savefig("output.pdf", dpi=400)
 
 
 if __name__ == "__main__":
